895d619a8257b9d1c21d97107662794b0312157e
AlexIvan-AG <wolfodonnell1934@gmail.com>
2023-09-09 14:35:29 -0500

    inicio

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php require 'controlador.php';?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Hash</title>
+</head>
+<body>
+    <form id="primero" action="" method="POST" autocomplete="off" >
+        <h1>Registro de Usuario</h1>
+
+        <label id="lab1"for="">Usuario</label>
+        <input id="inp1" type="text" name="usuario_usu" id="">
+
+        <label id="l1" for="">Password</label>
+        <input id="i1" type="password" name="password_usu" id="">
+        
+        <button id="b1" type="submit" name="registro">Registro</button>
+    </form>
+
+
+    <form id="segundo" action="" method="POST" autocomplete="off" >
+        <h1>Registro de Usuario</h1>
+
+        <label for="lab2">Usuario</label>
+        <input id="inp2" type="text" name="usuario_usu" id="">
+
+        <label id="l2" for="">Password</label>
+        <input id="i2" type="password" name="password_usu" id="">
+        
+        <button id="b2" type="submit" name="registro">Registro</button>
+    </form>
+
+
+    <form id="tercero" action="" method="POST" autocomplete="off" >
+        <h1>Registro de Usuario</h1>
+
+        <label id="lab3" for="">Usuario</label>
+        <input id="inp3" type="text" name="usuario_usu" id="">
+
+        <label id="l3" for="">Password</label>
+        <input id="i3" type="password" name="password_usu" id="">
+        
+        <button id="b3" type="submit" name="registro">Registro</button>
+    </form>
+</body>
+</html>
